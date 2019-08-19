bdee6fc9c16c34be2f2155242f2c7237b5abbca2
KasunW <kasun.w@duosoftware.com>
2019-08-19 13:49:27 +0530

    Revert "Revert "Additional cover added""
    This reverts commit 40ad110cc90ab030772276648ad104fb131f17e9.

---
+++ controllers/janashakthi.controller.js
@@ -0,0 +1,8 @@
+/* use strickt */
+
+app.controller('JanashakthiController', ['$scope', '$rootScope', janashakthiController]);
+
+function janashakthiController($scope, $rootScope) {
+    debugger
+    $rootScope.processing = false;
+}
